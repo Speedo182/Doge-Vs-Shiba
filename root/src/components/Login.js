17c7d0e5969f35979e396fdaede5794432ff85a1
Command Church <123043158+Speedo182@users.noreply.github.com>
2023-03-06 16:15:25 +1100

    Create Login.js

---
+++ root/src/components/Login.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (username === 'doge' && password === 'shiba') {
+      history.push('/game');
+    } else {
+      alert('Invalid username or password');
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
